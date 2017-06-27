ac77c631ceb99531096f4ce5e3402c8f8e9d644e
Doug Hellmann <doug@doughellmann.com>
2017-06-27 18:45:03 -0400

    copy the results to my web server
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ cd $(dirname $0)
 ./gen-burndown.py
 git add data.csv
 git commit -m "Updated csv"
+rsync -av --exclude config.ini data.csv index.html doughellmann.com:~/doughellmann.com/doc-migration/
