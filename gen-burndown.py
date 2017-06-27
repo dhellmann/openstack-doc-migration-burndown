f66ed200fa620b1dab08b1ebdf8c5de87d6708aa
Doug Hellmann <doug@doughellmann.com>
2017-06-27 18:36:29 -0400

    rewrite the script to count doc-migration changes
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ gen-burndown.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import csv
+import collections
+import time
+import os
+import configparser
+import json
+
+import requests
+from requests.auth import HTTPDigestAuth
+
+TOP = 'nova/api-ref/source'
+
+PROJECT_SITE = "https://review.openstack.org/changes/"
+QUERY = "q=topic:doc-migration"
+URL = "%s?%s" % (PROJECT_SITE, QUERY)
+
+
+def _parse_content(resp, debug=False):
+    # slice out the "safety characters"
+    if resp.content[:4] == b")]}'":
+        content = resp.content[5:]
+        if debug:
+            print("Response from Gerrit:\n")
+            print(content)
+        return json.loads(content.decode('utf-8'))
+    else:
+        print('could not parse response')
+        return resp.content
+
+
+def fetch_data(url, debug=False):
+    print('fetching {}'.format(url))
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    user = config.get('default', 'user')
+    password = config.get('default', 'password')
+    auth = HTTPDigestAuth(user, password)
+    resp = requests.get(url, auth=auth)
+    return _parse_content(resp, debug)
+    # return json.loads(_parse_content(resp, debug).decode('utf-8'))
+
+observed_repos = set()
+status_counts = collections.Counter()
+
+relevant = fetch_data(URL)
+for review in relevant:
+    if review['status'] == 'ABANDONED':
+        continue
+    observed_repos.add(review['project'])
+    status_counts.update([review['status']])
+
+with open('expected_repos.txt', 'r', encoding='utf-8') as f:
+    expected_repos = set([line.strip() for line in f])
+
+unseen_repos = expected_repos - observed_repos
+not_started = len(unseen_repos)
+
+print('Found {} changes in review'.format(status_counts['NEW']))
+print('Found {} changes merged'.format(status_counts['MERGED']))
+print('Found {} repos not started'.format(not_started))
+
+if not os.path.exists('data.csv'):
+    with open('data.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(('date', 'Repos Not Started', 'Changes In Review', 'Changes Merged'))
+
+with open('data.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(
+        (int(time.time()),
+         not_started,
+         status_counts['NEW'],
+         status_counts['MERGED']),
+    )
--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 set -e
 
 cd $(dirname $0)
-pushd nova > /dev/null
-git pull 2>&1 > /dev/null
-popd > /dev/null
-./api-ref-burndown.py
+./gen-burndown.py
 git ci -m "Updated csv" > /dev/null
