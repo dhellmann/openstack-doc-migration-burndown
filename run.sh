164838631ab837e79eca083bfa730dc32c0473ef
Doug Hellmann <doug@doughellmann.com>
2017-06-28 18:16:15 -0400

    copy all of the data to the web server
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ cd $(dirname $0)
 git add data.* notstarted.json
 git commit -m "Updated csv"
 #rsync -av --exclude config.ini data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
-scp -i ~/.ssh/id_rsa-backups data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
+scp -i ~/.ssh/id_rsa-backups data.* notstarted.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
