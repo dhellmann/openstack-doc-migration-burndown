49c4892d47fa749cff95092b6f5381d6b401460f
Doug Hellmann <doug@doughellmann.com>
2017-06-28 10:18:05 -0400

    switch to scp for pushing updates
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ cd $(dirname $0)
 ./gen-burndown.py
 git add data.*
 git commit -m "Updated csv"
-rsync -av --exclude config.ini data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
+#rsync -av --exclude config.ini data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
+scp -i ~/.ssh/id_rsa-backups data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
