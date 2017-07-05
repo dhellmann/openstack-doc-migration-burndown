65de41396c077d66e1b90cf4853e9760f8e347e0
Doug Hellmann <doug@doughellmann.com>
2017-07-05 08:42:56 -0400

    use UTC for timestamp of last updated
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ date
 
 cd $(dirname $0)
 ./gen-burndown.py
-sed -i "s/Last updated:.*/Last updated: $(date)/" index.html
+sed -i "s/Last updated:.*/Last updated: $(date -u)/" index.html
 git add data.* notstarted.json index.html
 git commit -m "Updated csv"
 scp -i ~/.ssh/id_rsa-backups data.* notstarted.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
