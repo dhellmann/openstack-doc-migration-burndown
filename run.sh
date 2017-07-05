f6695b3a28308a228dc57c8167b0aabe3eb2a044
Doug Hellmann <doug@doughellmann.com>
2017-07-05 08:41:53 -0400

    delete old rsync command
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ cd $(dirname $0)
 ./gen-burndown.py
 git add data.* notstarted.json
 git commit -m "Updated csv"
-#rsync -av --exclude config.ini data.* index.html doughellmann.com:~/doughellmann.com/doc-migration/
 scp -i ~/.ssh/id_rsa-backups data.* notstarted.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
