01ca414b799fe92cbc0cd93b7045d16353deccbc
Doug Hellmann <doug@doughellmann.com>
2017-07-05 08:42:02 -0400

    add date of run to index.html
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ date
 
 cd $(dirname $0)
 ./gen-burndown.py
-git add data.* notstarted.json
+sed -i "s/Last updated:.*/Last updated: $(date)/" index.html
+git add data.* notstarted.json index.html
 git commit -m "Updated csv"
 scp -i ~/.ssh/id_rsa-backups data.* notstarted.json index.html doughellmann.com:~/doughellmann.com/doc-migration/
